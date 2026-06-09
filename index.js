69ca292bcb765997bbca7c446e3088ceb494aa79
bayron-boop <robloxroblo897@gmail.com>
2026-06-08 19:40:23 -0500

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Bot WhatsApp online w 🔥');
-});
-
-app.listen(PORT, () => console.log(`Puerto ${PORT}`));
